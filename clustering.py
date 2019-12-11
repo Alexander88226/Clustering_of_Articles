94c1854aba86c8138264d3a0c937a37588624695
figaro <figaro@users.noreply.github.com>
2019-12-11 18:46:17 +1000

    output clustering result for stemming, lemmatizing, both of these.

--- clustering.py
+++ clustering.py
@@ -28,168 +28,156 @@ from sklearn.pipeline import make_pipeline
 sns.set_context('poster')
 sns.set_color_codes()
 plot_kwds = {'alpha' : 0.8, 's' : 80, 'linewidths':0}
-# 
-
-
-dataSetDir2 = os.path.join(os.getcwd(), "dataset")
-
-
-#  load our data-----------------------------------------
-all_data = datasets.load_files(dataSetDir2, 
-    description=None, load_content=True, encoding='utf-8', shuffle=False)
-#--------------------------------------------------------------
-labels = all_data.target
-
-
-"""
-This is a function that will count the number of times each word in the
-dataset occurs and project that count into a vector(TF-IDF vector). 
-"""
-count_vectorizer = TfidfVectorizer(stop_words='english')
-
-X = count_vectorizer.fit_transform(raw_documents=all_data.data)
-
-"""
-export the feature names
-"""
-feature_definition_file = 'feature_definition.data'
-
-feature_definition_file_handle = open(feature_definition_file, 'w') 
-id = 0
-for name in count_vectorizer.get_feature_names():
-    id = id + 1
-    feature_definition_file_handle.write("%d, %s\n"%(id, name))
-feature_definition_file_handle.close()
-
-"""
-export the TF-IDF vectors table into csv file
-"""
-results = pd.DataFrame(X.toarray(), columns=count_vectorizer.get_feature_names())
-
-results.to_csv("1.csv", encoding='utf-8')
-#--------------------------------------------------------------
-
-
-print("n_samples: %d, n_features: %d" % X.shape)
-
-print("Performing dimensionality reduction using LSA")
-
-# Vectorizer results are normalized, which makes KMeans behave as
-# spherical k-means for better results. Since LSA/SVD results are
-# not normalized, we have to redo the normalization.
-
-
-svd = TruncatedSVD(n_components=200)
-normalizer = Normalizer(copy=False)
-lsa = make_pipeline(svd, normalizer)
-
-X = lsa.fit_transform(X)
-
-
-explained_variance = svd.explained_variance_ratio_.sum()
-print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
-
-# print(X.shape)
-
-"""
-export the TF-IDF vectors table into csv file
-"""
-results = pd.DataFrame(X)
-
-results.to_csv("2.csv", encoding='utf-8')
-#--------------------------------------------------------------
-
-
-
-
-
-
-"""
-k-means clustering 
-"""
 
 k_clusters = 17
-km = KMeans(n_clusters=k_clusters, random_state=42)
-km.fit(X)
-# clusters = km.predict(X)
+
+def kmeans_clustering(datasetDir, preprocessing):
+    #  load our data-----------------------------------------
+    all_data = datasets.load_files(dataSetDir2, 
+        description=None, load_content=True, encoding='utf-8', shuffle=False)
+    #--------------------------------------------------------------
+    labels = all_data.target
 
 
-order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-centers_cluster_file = "centers.data"
-centers_cluster_handle = open(centers_cluster_file, 'w') 
-id = 0
-for center in order_centroids:
-    id += 1
-    centers_cluster_handle.write("%d, %s\n"%(id, center))
-centers_cluster_handle.close()
+    """
+    This is a function that will count the number of times each word in the
+    dataset occurs and project that count into a vector(TF-IDF vector). 
+    """
+    count_vectorizer = TfidfVectorizer(stop_words='english')
+
+    X = count_vectorizer.fit_transform(raw_documents=all_data.data)
+
+    """
+    export the feature names
+    """
+    feature_definition_file = preprocessing + '_feature_definition.data'
+
+    feature_definition_file_handle = open(feature_definition_file, 'w') 
+    id = 0
+    for name in count_vectorizer.get_feature_names():
+        id = id + 1
+        feature_definition_file_handle.write("%d, %s\n"%(id, name))
+    feature_definition_file_handle.close()
+
+    """
+    export the TF-IDF vectors table into csv file
+    """
+    results = pd.DataFrame(X.toarray(), columns=count_vectorizer.get_feature_names())
+
+    results.to_csv(preprocessing+"1.csv", encoding='utf-8')
+    #--------------------------------------------------------------
 
 
-terms = count_vectorizer.get_feature_names()
-#--------------------------------------------------------------
+    print("n_samples: %d, n_features: %d" % X.shape)
 
-labels = km.predict(X);
-palette = sns.color_palette('deep', np.unique(labels).max() + 1)
-colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
-plt.scatter(X.T[0], X.T[1], c=colors, **plot_kwds)
-frame = plt.gca()
-frame.axes.get_xaxis().set_visible(False)
-frame.axes.get_yaxis().set_visible(False)
-plt.show()
+    print("Performing dimensionality reduction using LSA")
+
+    # Vectorizer results are normalized, which makes KMeans behave as
+    # spherical k-means for better results. Since LSA/SVD results are
+    # not normalized, we have to redo the normalization.
 
 
-"""
-output the filenames of each clusters, order by distance from center to each sample vector
-"""
-clustering_result_file = 'clustering_result.data'
-clustering_file_handle = open(clustering_result_file, 'w') 
+    svd = TruncatedSVD(n_components=200)
+    normalizer = Normalizer(copy=False)
+    lsa = make_pipeline(svd, normalizer)
 
-"""
-show the labels of cluster data
-"""
+    X = lsa.fit_transform(X)
 
-original_space_centroids = svd.inverse_transform(km.cluster_centers_)
-order_centroids = original_space_centroids.argsort()[:, ::-1]
-cluster_labels_list = []
-# for i in range(k_clusters):
-#     cluster_label = []
-#     print("Cluster %d:" % i, end='')
-#     for ind in order_centroids[i, :10]:
-#         cluster_label.append(terms[ind])
-#     # cluster_label.join(terms[ind] for ind in order_centroids[i, :10])
-#     cluster_labels_list.append(cluster_label)
-# print(cluster_labels_list)
 
-# print(km.labels_)
+    explained_variance = svd.explained_variance_ratio_.sum()
+    print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
 
-for i in range(0, k_clusters):
-    d = km.transform(X)[:,i]
-    cluster_label = []
-    print("Cluster %d:" % i, end='')
-    for ind in order_centroids[i, :10]:
-        cluster_label.append(terms[ind])
-    print(cluster_label)
-    clustering_file_handle.write("cluster %d, label %s\n"%(i, cluster_label))
-    distances = []                      # distance list for each cluster
-    fileindexs = []                     # filename index for each cluster
-    counts_of_cluster = 0               # sample(file) count of each cluster
+    # print(X.shape)
 
-    for ind in range(len(labels)):
-        if i == km.labels_[ind]:
-            distances.append(d[ind])
-            fileindexs.append(ind)
-            counts_of_cluster += 1
+    """
+    export the TF-IDF vectors table into csv file
+    """
+    results = pd.DataFrame(X)
 
-    print('Cluster %d:' % i)
-    print('counts_of_cluster: %d' % counts_of_cluster)
-    
-    # sort by distance and get sorted index of distance list
-    indexs = np.argsort(distances)[::-1][:counts_of_cluster]
+    results.to_csv(preprocessing + "2.csv", encoding='utf-8')
+    #--------------------------------------------------------------
 
-    # export filenames for each cluster
-    for index in indexs:
-        clustering_file_handle.write("\t %s\n"%(all_data.filenames[fileindexs[index]]))
 
-clustering_file_handle.close()
+
+
+
+
+    """
+    k-means clustering 
+    """
+
+    km = KMeans(n_clusters=k_clusters, random_state=42)
+    km.fit(X)
+    # clusters = km.predict(X)
+
+
+    order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+    centers_cluster_file = preprocessing + "_centers.data"
+    centers_cluster_handle = open(centers_cluster_file, 'w') 
+    id = 0
+    for center in order_centroids:
+        id += 1
+        centers_cluster_handle.write("%d, %s\n"%(id, center))
+    centers_cluster_handle.close()
+
+
+    terms = count_vectorizer.get_feature_names()
+    #--------------------------------------------------------------
+
+    labels = km.predict(X);
+    palette = sns.color_palette('deep', np.unique(labels).max() + 1)
+    colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
+    plt.scatter(X.T[0], X.T[1], c=colors, **plot_kwds)
+    frame = plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+    """
+    output the filenames of each clusters, order by distance from center to each sample vector
+    """
+    clustering_result_file = preprocessing + '_clustering_result.data'
+    clustering_file_handle = open(clustering_result_file, 'w') 
+
+    """
+    show the labels of cluster data
+    """
+
+    original_space_centroids = svd.inverse_transform(km.cluster_centers_)
+    order_centroids = original_space_centroids.argsort()[:, ::-1]
+    cluster_labels_list = []
+
+    for i in range(0, k_clusters):
+        d = km.transform(X)[:,i]
+        cluster_label = []
+        print("Cluster %d:" % i, end='')
+        for ind in order_centroids[i, :10]:
+            cluster_label.append(terms[ind])
+        print(cluster_label)
+        clustering_file_handle.write("cluster %d, label %s\n"%(i, cluster_label))
+        distances = []                      # distance list for each cluster
+        fileindexs = []                     # filename index for each cluster
+        counts_of_cluster = 0               # sample(file) count of each cluster
+
+        for ind in range(len(labels)):
+            if i == km.labels_[ind]:
+                distances.append(d[ind])
+                fileindexs.append(ind)
+                counts_of_cluster += 1
+
+        print('Cluster %d:' % i)
+        print('counts_of_cluster: %d' % counts_of_cluster)
+        
+        # sort by distance and get sorted index of distance list
+        indexs = np.argsort(distances)[::-1][:counts_of_cluster]
+
+        # export filenames for each cluster
+        for index in indexs:
+            clustering_file_handle.write("\t %s\n"%(all_data.filenames[fileindexs[index]]))
+
+    clustering_file_handle.close()
+
 # print("Silhouette Coefficient: %0.3f"
 #       % metrics.silhouette_score(X, km.labels_, sample_size=1000))
 
@@ -215,3 +203,11 @@ clustering_file_handle.close()
 
 # print(prelabel)
 # print(distance)
+dataSetDir2 = os.path.join(os.getcwd(), "dataset")
+kmeans_clustering(dataSetDir2, "full_preprocessing")
+
+dataSetDir2 = os.path.join(os.getcwd(), "dataset_stemming")
+kmeans_clustering(dataSetDir2, "stemming")
+
+dataSetDir2 = os.path.join(os.getcwd(), "dataset_lemmatizing")
+kmeans_clustering(dataSetDir2, "lemmatizing")
